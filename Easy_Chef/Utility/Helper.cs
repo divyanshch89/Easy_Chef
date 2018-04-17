5138a239f12a144cde3a4bd433cd9f2fba50c725
Divyansh <divyanshch@outlook.com>
2018-04-17 01:07:25 -0500

    updated checkout view

---
+++ Easy_Chef/Utility/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easy_Chef.Utility
+{
+    public class Helper
+    {
+        public static string GenerateOrderNumber()
+        {
+            var result = "1111111111";
+            Random rand = new Random(DateTime.Now.Millisecond);
+            result = rand.Next().ToString();
+            return result;
+        }
+    }
+}
