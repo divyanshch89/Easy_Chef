197a4ee97591e4dcbc81b112f9cfba612ec76284
Divyansh <divyanshch@outlook.com>
2018-04-18 03:14:41 -0500

    added error controller

---
+++ Easy_Chef/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy_Chef.Controllers
+{
+    [Route("error")]
+    public class ErrorController : Controller
+    {
+        [Route("500")]
+        public IActionResult AppError()
+        {
+            return View();
+        }
+        [Route("404")]
+        public IActionResult PageNotFound()
+        {
+            string originalPath = "unknown";
+            if (HttpContext.Items.ContainsKey("originalPath"))
+            {
+                originalPath = HttpContext.Items["originalPath"] as string;
+            }
+            return View();
+        }
+    }
+}
